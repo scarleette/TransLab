48fc3c54352d07527b1acf7808eb30e3ced69a54
Scarlett <scar.antoine@gmail.com>
2018-08-04 00:01:06 -0400

    Funcion para crear usuarios en auth de Firebase

---
+++ assets/js/registro.js
@@ -0,0 +1,18 @@
+let userId;
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', e =>  {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    if(email.length != 0 && password.length != 0) {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(Response => {
+        userId  = Response;
+        console.log(userId);
+        console.log('ya envie los datos');  
+      }).catch((error)=>{
+        console.log(error);
+      });
+    }
+});
