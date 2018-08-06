2a6853916b6d3a7fe0644c95eee8dfa3c6b7283a
Scarlett <scar.antoine@gmail.com>
2018-08-06 09:34:56 -0400

    Cambios pequeños en funcion

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,20 +1,19 @@
 const formulario = document.getElementById('formulario');
 
 formulario.addEventListener('submit', e =>  {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    if(email.length != 0 && password.length != 0) {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(Response => {
-        userId  = Response;
-        console.log(userId);
-        location.href = '../html/login.html';
-        console.log('ya envie los datos');  
-      }).catch((error)=>{
-        console.log(error);
-      });
-    }
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  if(email.length != 0 && password.length != 0) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(Response => {
+      const user = Response;
+      return user;
+      console.log('ya envie los datos');
+    }).then(user => {
+      location.href = '../html/login.html';
+    }).catch((error)=>{
+    console.log(error);
+    });
+  }
 });
-
-console.log(userId);
